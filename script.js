bb86e1d7c05951f2a6aaf830cc2ca1f978ec6f4c
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-05-29 18:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let prom = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve();
+	},1000);
+});
+prom.then(()=>{
+	const div = document.getElementById("output");
+	div.innerText="Hello, world!"
+})
